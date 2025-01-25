dcb498f077b1a4889172fcf6ca0654eac9dd422f
Bartu Baş <bartubas99@gmail.com>
2025-01-26 00:44:09 +0300

    GamePhysicsMask

--- Assets/AAA/Code/EnemySpawner/GamePhysicsLayer.cs
+++ Assets/AAA/Code/EnemySpawner/GamePhysicsLayer.cs
@@ -8,4 +8,13 @@ namespace Game
 		Ground = 3,
 		Projectile = 4,
 	}
+
+	public enum GamePhysicsMask
+	{
+		Default = 1 << GamePhysicsLayer.Default,
+		Player = 1 << GamePhysicsLayer.Player,
+		Enemy = 1 << GamePhysicsLayer.Enemy,
+		Ground = 1 << GamePhysicsLayer.Ground,
+		Projectile = 1 << GamePhysicsLayer.Projectile,
+	}
 }
