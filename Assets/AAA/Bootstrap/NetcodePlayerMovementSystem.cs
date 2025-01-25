a8cda9d7291a2318dd1f28f842c8da3d65b202a6
Bartu Baş <bartubas99@gmail.com>
2025-01-25 21:14:07 +0300

    Fix time

--- Assets/AAA/Bootstrap/NetcodePlayerMovementSystem.cs
+++ Assets/AAA/Bootstrap/NetcodePlayerMovementSystem.cs
@@ -17,11 +17,13 @@ partial struct NetcodePlayerMovementSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
+        var dt = SystemAPI.Time.DeltaTime;
+        
         foreach (var (netcodePlayerInput, localTransform) in SystemAPI.Query<RefRO<NetcodePlayerInput>, RefRW<LocalTransform>>().WithAll<Simulate>())
         {
             float3 movementInput = new float3(netcodePlayerInput.ValueRO.MovementInputVector.x, 0, netcodePlayerInput.ValueRO.MovementInputVector.y);
             var runMultiplier = netcodePlayerInput.ValueRO.RunInputEvent.IsSet ? 2f : 1f;
-            localTransform.ValueRW.Position +=  movementInput * Time.deltaTime * 10 * runMultiplier;
+            localTransform.ValueRW.Position +=  movementInput * dt * 10 * runMultiplier;
         }
     }
 
