dd81c7c0d6a12a93bd50bf0d0a570f2830e40d36
Bartu Baş <bartubas99@gmail.com>
2025-01-25 20:37:49 +0300

    HealthSystem initial logic

--- Assets/AAA/Bootstrap/HealthSystem/HealthSystem.cs
+++ Assets/AAA/Bootstrap/HealthSystem/HealthSystem.cs
@@ -1,4 +1,5 @@
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 
 namespace Game
@@ -9,20 +10,36 @@ namespace Game
 		[BurstCompile]
 		public void OnCreate(ref SystemState state)
 		{
-			
 		}
 
 		[BurstCompile]
 		public void OnUpdate(ref SystemState state)
 		{
+			var ecb = new EntityCommandBuffer(Allocator.Temp);
 
+			foreach (var (health, damageBuffer, entity) in
+			         SystemAPI.Query<RefRW<Health>, DynamicBuffer<DamageBuffer>>().WithEntityAccess())
+			{
+				for (int i = 0; i < damageBuffer.Length; i++)
+				{
+					health.ValueRW.Value -= damageBuffer[i].Damage;
+				}
+
+				if (health.ValueRW.Value <= 0f)
+				{
+					ecb.SetComponentEnabled<Alive>(entity, false);
+				}
+
+
+				damageBuffer.Clear();
+			}
 			
+			ecb.Playback(state.EntityManager);
 		}
 
 		[BurstCompile]
 		public void OnDestroy(ref SystemState state)
 		{
-
 		}
 	}
 }
