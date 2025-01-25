352fdb2f19e3ea002fdc1b63e541a130b03766d5
yigitcanoksuz <yigitcanoksuz@gmail.com>
2025-01-26 00:52:25 +0300

    WIP lobby implementation with host client connection

--- Assets/AAA/Lobby/LobbyHud.cs
+++ Assets/AAA/Lobby/LobbyHud.cs
@@ -1,3 +1,8 @@
+using System;
+using TMPro;
+using Unity.Entities;
+using Unity.NetCode;
+using Unity.Networking.Transport;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,15 +11,93 @@ namespace AAA.Lobby
     public class LobbyHud : MonoBehaviour
     {
         [SerializeField] private Button _hostButton;
+        [SerializeField] private Button _joinButton;
+        [SerializeField] private TMP_InputField _ipAddressInput;
         
         private void Start()
         {
             _hostButton.onClick.AddListener(OnHostClicked);
+            _joinButton.onClick.AddListener(OnJoinClicked);
+        }
+
+        private void OnJoinClicked()
+        {
+            if (CheckIfAlreadyHostedOrConnected())
+            {
+                return;
+            }
+            
+            var client = ClientServerBootstrap.CreateClientWorld("ClientWorld");
+            
+            if (World.DefaultGameObjectInjectionWorld == null)
+                World.DefaultGameObjectInjectionWorld = client;
+            
+            var ip = _ipAddressInput.text;
+            var ep = NetworkEndpoint.Parse(ip, 40900);
+            {
+                using var drvQuery = client.EntityManager.CreateEntityQuery(ComponentType.ReadWrite<NetworkStreamDriver>());
+                drvQuery.GetSingletonRW<NetworkStreamDriver>().ValueRW.Connect(client.EntityManager, ep);
+            }
         }
 
         private void OnHostClicked()
         {
             Debug.Log("Host clicked");
+
+            if (CheckIfAlreadyHostedOrConnected())
+            {
+                return;
+            }
+            
+            var client = ClientServerBootstrap.CreateClientWorld("ClientWorld");
+            var server = ClientServerBootstrap.CreateServerWorld("ServerWorld");
+            
+            
+            if (World.DefaultGameObjectInjectionWorld == null)
+                World.DefaultGameObjectInjectionWorld = server;
+
+            ushort port = 40900;
+
+            NetworkEndpoint ep = NetworkEndpoint.AnyIpv4.WithPort(port);
+            {
+                using var drvQuery = server.EntityManager.CreateEntityQuery(ComponentType.ReadWrite<NetworkStreamDriver>());
+
+                var driver = drvQuery.GetSingletonRW<NetworkStreamDriver>();
+                driver.ValueRW.RequireConnectionApproval = false;
+                var success = driver.ValueRW.Listen(ep);
+                
+                if (!success)
+                {
+                    Debug.LogError("Failed to listen on port " + port);
+                    return;
+                }
+            }
+
+            ep = NetworkEndpoint.LoopbackIpv4.WithPort(port);
+            {
+                using var drvQuery = client.EntityManager.CreateEntityQuery(ComponentType.ReadWrite<NetworkStreamDriver>());
+                drvQuery.GetSingletonRW<NetworkStreamDriver>().ValueRW.Connect(client.EntityManager, ep);
+            }
+            
+            Debug.Log("Hosted");
+        }
+
+        private bool CheckIfAlreadyHostedOrConnected()
+        {
+            if (ClientServerBootstrap.ClientWorld != null && ClientServerBootstrap.ServerWorld != null)
+            {
+                Debug.Log("Already hosted");
+                return true;
+            }
+            else if (ClientServerBootstrap.ClientWorld != null)
+            {
+                Debug.Log("Already connected");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
--- Assets/AAA/Lobby/LobbySystem.cs
+++ Assets/AAA/Lobby/LobbySystem.cs
@@ -1,18 +1,41 @@
-using System;
+using Unity.Collections;
 using Unity.Entities;
+using Unity.NetCode;
 using UnityEngine;
 
 namespace AAA.Lobby
 {
     
-    [WorldSystemFilter(WorldSystemFilterFlags.LocalSimulation)]
+    [WorldSystemFilter(WorldSystemFilterFlags.ClientSimulation | WorldSystemFilterFlags.ServerSimulation)]
     [UpdateInGroup(typeof(LateSimulationSystemGroup))]
-    [DisableAutoCreation]
-    public partial class LobbySystem : SystemBase
+    partial struct LobbySystem : ISystem
     {
-        protected override void OnUpdate()
+        public void OnCreate(ref SystemState state)
         {
-           Debug.Log("LobbySystem.OnUpdate");
+            state.RequireForUpdate<NetworkStreamConnection>();
         }
+
+        public void OnUpdate(ref SystemState state)
+        {
+            var buffer = new EntityCommandBuffer(Allocator.Temp);
+            foreach (var (connection,entity) in SystemAPI.Query<RefRO<NetworkStreamConnection>>().WithNone<ConnectedClient>().WithEntityAccess())
+            {
+                if (connection.ValueRO.CurrentState == ConnectionState.State.Connected)
+                {
+                    buffer.AddComponent(entity, new ConnectedClient());
+                    Debug.Log("Client connected");
+
+                    if (state.World.IsServer())
+                    {
+                        Debug.Log("Client connected to the server");
+                    }
+                }
+            }
+            buffer.Playback(state.EntityManager);
+        }
+    }
+    
+    public struct ConnectedClient : IComponentData
+    {
     }
 }
