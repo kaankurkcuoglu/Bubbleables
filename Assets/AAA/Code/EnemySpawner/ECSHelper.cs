88cf20130b2a7c4bd0e6e055265e5b11a96eb2ef
Bartu Baş <bartubas99@gmail.com>
2025-01-25 23:53:50 +0300

    Add helpers

--- Assets/AAA/Code/EnemySpawner/ECSHelper.cs
+++ Assets/AAA/Code/EnemySpawner/ECSHelper.cs
@@ -1,7 +1,8 @@
+using System;
 using Unity.Collections;
 using Unity.Entities;
-using Unity.Mathematics;
 using Unity.NetCode;
+using Random = Unity.Mathematics.Random;
 
 namespace Game
 {
@@ -16,6 +17,32 @@ namespace Game
 			var entities = query.ToEntityArray(Allocator.Temp);
 			var entityIdx = random.NextInt(0, entities.Length);
 			return entities[entityIdx];
+		}
+
+		public static EntityManager GetEntityManager()
+		{
+			return GetClientWorld().EntityManager;
+		}
+
+		public static Entity GetLocalPlayerEntity()
+		{
+			var em = GetClientWorld().EntityManager;
+			var query = new EntityQueryBuilder(Allocator.Temp)
+				.WithAll<GhostOwnerIsLocal>()
+				.Build(em);
+
+			return query.ToEntityArray(Allocator.Temp)[0];
+		}
+
+		public static World GetClientWorld()
+		{
+			foreach (var world in World.All)
+			{
+				if (world.IsClient())
+					return world;
+			}
+
+			throw new Exception("There's no Client World");
 		}
 	}
 }
